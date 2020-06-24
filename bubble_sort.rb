42edee9319df7ad164b2a7c19ee309a13f52a86d
tiberiufrat <tiberiu.fratila@gmail.com>
2020-06-24 21:29:02 +0300

    add method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,3 +1,9 @@
 def bubble_sort(array)
-  
+  array.each_index do |index|
+    if array[index] > array[index + 1]
+      aux = array[index]
+      array[index] = array[index + 1]
+      array[index + 1] = aux
+    end
+  end
 end
