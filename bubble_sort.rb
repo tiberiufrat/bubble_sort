1da0344a254905b5ca5384f80343e256d03742b5
tiberiufrat <tiberiu.fratila@gmail.com>
2020-06-24 21:44:36 +0300

    changes

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,9 +1,17 @@
 def bubble_sort(array)
-  array.each_index do |index|
-    if array[index] > array[index + 1]
-      aux = array[index]
-      array[index] = array[index + 1]
-      array[index + 1] = aux
+  swapped = true
+  while swapped
+    swapped = false
+    array.slice(0, array.length - 1).each_index do |i|
+      if array[i] > array[i + 1]
+        aux = array[i]
+        array[i] = array[i + 1]
+        array[i + 1] = aux
+        swapped = true
+      end
     end
   end
+  array
 end
+
+print bubble_sort([4,3,78,2,0,2])
